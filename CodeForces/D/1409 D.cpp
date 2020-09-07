00e50139e2f4a8f16fcc6f54ba4bee031f2507eb
rahul-goel <rahulgoellko@gmail.com>
2020-09-07 19:13:28 +0530

    div 3

---
+++ CodeForces/D/1409 D.cpp
@@ -0,0 +1,103 @@
+/*
+    Created by Rahul Goel.
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+/*******************************************************************************/
+using namespace std;
+using namespace __gnu_pbds;
+template < typename T >
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+/*******************************************************************************/
+using ll = long long;
+using ld = long double;
+const ll MOD = 1000000007;
+// const ll MOD = 998244353;
+const int INF = 1e9;
+const ll LINF = 1e18;
+/*******************************************************************************/
+ll mod_sum() { return 0LL; }
+template < typename T, typename... Args >
+T mod_sum(T a, Args... args) { return ((a + mod_sum(args...))%MOD + MOD)%MOD; }
+/*******************************************************************************/
+ll mod_prod() { return 1LL; }
+template< typename T, typename... Args >
+T mod_prod(T a, Args... args) { return (a * mod_prod(args...))%MOD; }
+/*******************************************************************************/
+#ifdef ONLINE_JUDGE
+#define endl '\n'
+#endif
+#define fastio          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(c)          (c).begin(), (c).end()
+#define present(c,x)    ((c).find(x) != (c).end())
+#define cpresent(c,x)   (find(all(c),x) != (c).end())
+#define uniq(c)         (c).resize(distance((c).begin(), unique(all(c))))
+#define min_pq(t)       priority_queue < t, vector < t >, greater < t > >
+#define max_pq(t)       priority_queue < t >
+#define pb              push_back
+#define ff              first
+#define ss              second
+/*******************************************************************************/
+using pii = pair < ll, ll >;
+using vi = vector < ll >;
+using vb = vector < bool >;
+using vvi = vector < vector < ll > >;
+using vvb = vector < vector < bool > >;
+using vpii = vector < pii >;
+using vvpii = vector < vector < pii > >;
+/*******************************************************************************/
+//.-.. . -. -.- .- .. ... .-.. --- ...- .
+/*
+    Code begins after this.
+*/
+
+ll sod(ll n) {
+    ll ret = 0;
+    while (n) {
+        ret += n % 10;
+        n /= 10;
+    }
+    return ret;
+}
+
+ll power(ll x, ll y) {
+    ll ret = 1;
+    for (ll i = 0; i < y; i++) {
+        ret *= x;
+    }
+    return ret;
+}
+
+ll solve() {
+    ll n, s;
+    cin >> n >> s;
+    ll ans = 0;
+
+    ll m = n;
+    for (ll i = 0; n; i++) {
+        if (sod(n) <= s) {
+            break;
+        }
+        if ((n / power(10, i)) % 10) {
+            ll del = (10 - ((n / power(10, i)) % 10)) * power(10, i);
+            n += del;
+            ans += del;
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
+
+signed main() {
+    fastio;
+
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
